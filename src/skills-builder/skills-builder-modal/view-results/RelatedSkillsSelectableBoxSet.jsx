c768561f3a30e8e7bc4ab43e333f03f9fce66c2d
Chris Deery <3932645+cdeery@users.noreply.github.com>
2023-07-14 17:23:14 +0000

    fix: use overflow css property

--- src/skills-builder/skills-builder-modal/view-results/RelatedSkillsSelectableBoxSet.jsx
+++ src/skills-builder/skills-builder-modal/view-results/RelatedSkillsSelectableBoxSet.jsx
@@ -1,15 +1,14 @@
-import { React, useContext } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  breakpoints, Button, Chip, OverflowScroll, OverflowScrollContext,
-  SelectableBox, Stack, useMediaQuery,
+  SelectableBox, Chip, Stack, useMediaQuery, breakpoints,
 } from '@edx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import messages from './messages';
 
 const RelatedSkillsSelectableBoxSet = ({ jobSkillsList, selectedJobTitle, onChange }) => {
   const { formatMessage } = useIntl();
-  const isExtraSmall = useMediaQuery({ maxWidth: breakpoints.medium.maxWidth });
+  const isExtraSmall = useMediaQuery({ maxWidth: breakpoints.extraSmall.maxWidth });
 
   const renderTopFiveSkills = (skills) => {
     const topFiveSkills = skills.sort((a, b) => b.significance - a.significance).slice(0, 5);
@@ -22,18 +21,22 @@ const RelatedSkillsSelectableBoxSet = ({ jobSkillsList, selectedJobTitle, onChan
     );
   };
 
-  const jobSelectableBoxList = (setPad = false) => {
-    const pad = setPad ? 'mr-2 flex-shrink-0' : null;
-    return (
-      jobSkillsList.map((job, i, { length }) => (
+  return (
+    <SelectableBox.Set
+      name="selected job title"
+      type="radio"
+      value={selectedJobTitle}
+      onChange={onChange}
+      columns={3}
+      className="overflow-scroll-medium"
+    >
+      {jobSkillsList.map(job => (
         <SelectableBox
           key={job.id}
           type="radio"
           value={job.name}
           aria-label={job.name}
           inputHidden={false}
-          // don't set pad on last item
-          className={(length - 1 !== i) ? pad : null} // what is the correct class to use here?
         >
           <p>{job.name}</p>
           <Stack gap={2} className="align-items-start">
@@ -41,87 +44,8 @@ const RelatedSkillsSelectableBoxSet = ({ jobSkillsList, selectedJobTitle, onChan
             {renderTopFiveSkills(job.skills)}
           </Stack>
         </SelectableBox>
-      ))
-    );
-  };
-
-  const renderSelectableBoxSet = () => {
-    return (
-      <SelectableBox.Set
-        name="selected job title"
-        type="radio"
-        value={selectedJobTitle}
-        onChange={onChange}
-        columns={3}
-      >
-        {jobSelectableBoxList()}
-      </SelectableBox.Set>
-    );
-  };
-
-  const OverflowScrollContent = () => {
-    const { setOverflowRef } = useContext(OverflowScrollContext);
-    return (
-      <div
-        ref={setOverflowRef}
-        className="d-flex"
-      >
-        <OverflowScroll.Items>
-          {jobSelectableBoxList(true)}
-        </OverflowScroll.Items>
-      </div>
-    );
-  };
-
-  const OverflowScrollControls = () => {
-    const {
-      isScrolledToStart,
-      isScrolledToEnd,
-      scrollToPrevious,
-      scrollToNext,
-    } = useContext(OverflowScrollContext);
-
-    return (
-      <div className="mb-3">
-        <Button
-          onClick={scrollToPrevious}
-          disabled={isScrolledToStart}
-          size="sm"
-          className="mr-2"
-        >
-          Previous
-        </Button>
-        <Button
-          onClick={scrollToNext}
-          disabled={isScrolledToEnd}
-          size="sm"
-        >
-          Next
-        </Button>
-      </div>
-    );
-  };
-
-  return (
-    <div>
-      {
-        isExtraSmall ? (
-          <OverflowScroll
-            ariaLabel="Select Job title to see recommended skills" // put in messages
-            hasInteractiveChildren
-            offset="2"
-            offsetType="fixed"
-          >
-            <OverflowScrollControls />
-            <OverflowScrollContent />
-          </OverflowScroll>
-        ) : (
-          <div>
-            {renderSelectableBoxSet()}
-          </div>
-        )
-      }
-    </div>
+      ))}
+    </SelectableBox.Set>
   );
 };
 
