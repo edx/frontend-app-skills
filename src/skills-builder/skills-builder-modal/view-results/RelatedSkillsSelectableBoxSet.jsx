2443b1da2a93efb2e7ff5b29fbe4dcc0bcfe7c79
Chris Deery <3932645+cdeery@users.noreply.github.com>
2023-07-14 17:25:08 +0000

    fix: clean up lint issues

--- src/skills-builder/skills-builder-modal/view-results/RelatedSkillsSelectableBoxSet.jsx
+++ src/skills-builder/skills-builder-modal/view-results/RelatedSkillsSelectableBoxSet.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  SelectableBox, Chip, Stack, useMediaQuery, breakpoints,
+  SelectableBox, Chip, Stack,
 } from '@edx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
 import messages from './messages';
 
 const RelatedSkillsSelectableBoxSet = ({ jobSkillsList, selectedJobTitle, onChange }) => {
   const { formatMessage } = useIntl();
-  const isExtraSmall = useMediaQuery({ maxWidth: breakpoints.extraSmall.maxWidth });
 
   const renderTopFiveSkills = (skills) => {
     const topFiveSkills = skills.sort((a, b) => b.significance - a.significance).slice(0, 5);
