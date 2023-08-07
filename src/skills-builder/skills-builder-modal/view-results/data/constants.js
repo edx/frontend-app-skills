4eed282f638ae1dd5ad42d3ef8e039b3fa97633f
Chris Deery <3932645+cdeery@users.noreply.github.com>
2023-08-07 21:14:09 +0000

    fix: change 2u_degree param for temp disable

--- src/skills-builder/skills-builder-modal/view-results/data/constants.js
+++ src/skills-builder/skills-builder-modal/view-results/data/constants.js
@@ -1,7 +1,7 @@
 export const COURSE = 'course';
 export const BOOT_CAMP = 'boot_camp';
 export const EXECUTIVE_EDUCATION = 'executive_education';
-export const DEGREE = '2U_degree';
+export const DEGREE = '2U_degrees';
 export const PROGRAM = 'program';
 
 // This array is used to determine the validity of product types as they are passed through the query string
